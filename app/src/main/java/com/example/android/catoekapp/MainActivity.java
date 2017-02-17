fb5615adfb68845536052dbdfe4522fb723160a1
Mahardika Nila Kandi <mahardikadik@gmail.com>
2017-02-18 00:32:52 +0700

    Commit Project Home Layout

---
+++ app/src/main/java/com/example/android/catoekapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.catoekapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button butt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        butt = (Button) findViewById(R.id.Signin);
+//        butt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, SigninActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
